df1ac3b32e02d452d82dd8c1c64d8b1cc3f0595a
Drew Mercer <japanexpress@gmail.com>
2017-09-19 10:50:52 -0400

    Updating schema and adding package:

--- schema.sql
+++ schema.sql
@@ -1,16 +1,22 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE database bamazon;
-
 USE bamazon;
-
-CREATE TABLE products (
-  item_id INT AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(100) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  item_sales DECIMAL(10,2) NOT NULL,
-  stock_quantity INT(10) NULL,
-  PRIMARY KEY (item_id)
+CREATE TABLE products
+(
+    item_id INT
+    AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR
+    (100) NOT NULL,
+  department_name VARCHAR
+    (100) NOT NULL,
+  price DECIMAL
+    (10,2) NOT NULL,
+  item_sales DECIMAL
+    (10,2) NOT NULL,
+  stock_quantity INT
+    (10) NULL,
+  PRIMARY KEY
+    (item_id)
 );
-
-SELECT * FROM products;
+    SELECT *
+    FROM products;
