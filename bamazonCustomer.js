595df67a4ce08aafb4b377010e3d1f24ab378f93
Drew Mercer <japanexpress@gmail.com>
2017-09-19 12:13:59 -0400

    Cleaning table labels & layout

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,8 +16,66 @@ connection.connect(function(err) {
 });
 
 function queryProducts() {
-  var query = 'SELECT item_id, product_name, price FROM products';
+  var query = 'SELECT item_id AS SKU, product_name AS "Product Description", price AS Price FROM products';
   connection.query(query, function(err, res) {
     console.table(res);
   });
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What product id would you like to buy?',
+        name: 'id'
+      },
+      {
+        type: 'input',
+        message: 'How many units would you like to buy?',
+        name: 'quantity'
+      }
+    ])
+    .then(function(order) {
+      var quantity = order.quantity;
+      var itemId = order.id;
+      connection.query('SELECT * FROM products WHERE id=' + itemId, function(
+        err,
+        selectedItem
+      ) {
+        if (err) throw err;
+        if (selectedItem[0].StockQuantity - quantity >= 0) {
+          // console.log("Bamazon's shelves have enough of that item ("selectedItem[0].ProductName + ")!");
+          console.log(
+            'Quantity in Stock: ' +
+              selectedItem[0].StockQuantity +
+              ' Order Quantity: ' +
+              quantity
+          );
+          console.log(
+            'You will be charged ' +
+              order.quantity * selectedItem[0].Price +
+              ' dollars.  Thank you for shopping at Bamazon.'
+          );
+          connection.query(
+            'UPDATE products SET StockQuantity=? WHERE id=?',
+            [selectedItem[0].StockQuantity - quantity, itemId],
+            function(err, inventory) {
+              if (err) throw err;
+              // Runs the prompt again, so the user can keep shopping.
+              queryProducts();
+            }
+          ); // Ends the code to remove item from inventory.
+        } else {
+          console.log(
+            'Insufficient inventory to fulfiull this order. Please order less of that item, as Bamazon only has ' +
+              selectedItem[0].StockQuantity +
+              ' ' +
+              selectedItem[0].ProductName +
+              ' in stock at this moment.'
+          );
+          queryProducts();
+        }
+      });
+    });
 }
+
+queryProducts();
