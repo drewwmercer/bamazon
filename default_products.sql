281e8b96f7a5a01713e790cf15b18c91b813f20b
Drew Mercer <japanexpress@gmail.com>
2017-09-19 10:14:08 -0400

    Stocking up bamazon

---
+++ default_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (item_description,department,item_price,item_qty)
+VALUES ('Fire TV Stick with Balexa Voice Remote | Streaming Media Player','Electronics',39.99,500000),
+        ('Becho Dot (2nd Generation) - Black','Electronics',49.99,300000),
+        ('Principles: Life and Work','Books',18.00,85),
+        ('It: A Novel','Books',11.99,86),
+        ('The Official SAT Study Guide, 2018 Edition','Books',12.60,1600),
+        
