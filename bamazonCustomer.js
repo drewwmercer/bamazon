c3642362c6b8964d3bf6c720ec9c979f498ab0b4
Drew Mercer <japanexpress@gmail.com>
2017-09-19 10:55:17 -0400

    Setting up customer logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,11 @@
 var mysql = require('mysql');
 var table = require('console.table');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 8889,
+  user: 'root',
+  password: 'root',
+  database: 'bamazon'
+});
