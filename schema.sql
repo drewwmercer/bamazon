342d6c9b5eae28ffb417ddc1a5a93b62e6bf2d31
Drew Mercer <japanexpress@gmail.com>
2017-09-19 10:04:34 -0400

    Working on db schema

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE database bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_num INT NOT NULL,
-  item_description VARCHAR(100) NULL,
-  department VARCHAR(100) NULL,
-  item_price INT NULL,
-  item_sales DECIMAL(10,4) NULL,
-  item_qty DECIMAL(10,4) NULL,
+  item_num INT AUTO_INCREMENT NOT NULL,
+  item_description VARCHAR(100) NOT NULL,
+  department VARCHAR(100) NOT NULL,
+  item_price DECIMAL(10,2) NOT NULL,
+  item_sales DECIMAL(10,2) NOT NULL,
+  item_qty INT(10) NULL,
   PRIMARY KEY (item_num)
 );
 
