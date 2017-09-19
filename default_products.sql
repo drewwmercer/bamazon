953e957016280bfc3df7f700e8a404b5c55e9f2b
Drew Mercer <japanexpress@gmail.com>
2017-09-19 10:21:31 -0400

    Stocking up bamazon

--- default_products.sql
+++ default_products.sql
@@ -1,7 +1,16 @@
-INSERT INTO products (item_description,department,item_price,item_qty)
-VALUES ('Fire TV Stick with Balexa Voice Remote | Streaming Media Player','Electronics',39.99,500000),
-        ('Becho Dot (2nd Generation) - Black','Electronics',49.99,300000),
-        ('Principles: Life and Work','Books',18.00,85),
-        ('It: A Novel','Books',11.99,86),
-        ('The Official SAT Study Guide, 2018 Edition','Books',12.60,1600),
+INSERT INTO products
+    (item_description,department,item_price,item_qty)
+VALUES
+    ('Fire TV Stick with Balexa Voice Remote | Streaming Media Player', 'Electronics', 39.99, 500000),
+    ('Becho Dot (2nd Generation) - Black', 'Electronics', 49.99, 300000),
+    ('Principles: Life and Work', 'Books', 18.00, 85),
+    ('It: A Novel', 'Books', 11.99, 86),
+    ('The Official SAT Study Guide, 2018 Edition', 'Books', 12.60, 1600),
+    ('Hatchimals - CollEGGtibles 4-Pack + Bonus', 'Toys & Games', 9.99, 4),
+    ('WowWee Fingerlings Mia Purple Baby Monkey', 'Toys & Games', 7.68, 99),
+    ('Dr. Elsey Precious Cat Ultra Cat Litter', 'Pet Supplies', 13.68, 400),
+    ('BamazonBasics Pet Training and Puppy Pads', 'Pet Supplies', 16.99, 1000),
+    ('GREENIES PILL POCKETS Chicken Flavor Treats', 'Pet Supplies', 16.99, 250),
+    ('Mr. Clean Magic Eraser Cleaning Pads', 'Industrial & Scientific', 6.99, 35),
+    ('Etekcity Digital Hanging Luggage Scale', 'Industrial & Scientific', 9.97, 86);
         
