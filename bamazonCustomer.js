6f7b90da186d72a8611414698030123fda886ab7
Drew Mercer <japanexpress@gmail.com>
2017-09-21 00:40:40 -0400

    Adding validation to quantity prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,61 +22,76 @@ function queryProducts() {
     console.table(res);
     goShopping();
   });
-};
+}
+
 function goShopping() {
-  inquirer.prompt([
+  inquirer
+    .prompt([
       {
         type: 'input',
         message: 'What product id would you like to buy?',
         name: 'id',
         validate: function(value) {
           if (isNaN(value) == false) {
-            return true
+            return true;
           } else {
-            return false
+            return false;
           }
         }
       },
       {
         type: 'input',
         message: 'How many units would you like to buy?',
-        name: 'quantity'
+        name: 'quantity',
+        validate: function(value) {
+          if (isNaN(value) == false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
       }
-    ]).then(function(order) {
+    ])
+    .then(function(order) {
       var quantity = order.quantity;
       var itemId = order.id;
-      connection.query('SELECT * FROM products WHERE id=' + itemId, function(
-        err,
-        selectedItem
-      ) {
-        if (err) throw err;
-        if (selectedItem[0].StockQuantity - quantity >= 0) {
-          // console.log("Bamazon's shelves have enough of that item ("selectedItem[0].ProductName + ")!");
-          console.log(
-            'Quantity in Stock: ' +
-              selectedItem[0].StockQuantity +
-              ' Order Quantity: ' +
-              quantity
-          );
-          console.log(
-            'You will be charged ' +
-              order.quantity * selectedItem[0].Price +
-              ' dollars.  Thank you for shopping at Bamazon.'
-          );
-          connection.query(
-            'UPDATE products SET StockQuantity=? WHERE id=?',
-            [selectedItem[0].StockQuantity - quantity, itemId],
-            function(err, inventory) {
-              if (err) throw err;
-              queryProducts();
-            });
-        } else {
-          console.log(
-            'Insufficient inventory to fulfill this order. Please order less of that item, as Bamazon only has ' + selectedItem[0].StockQuantity + ' ' + selectedItem[0].ProductName + ' in stock at this moment.'
-          );
-          queryProducts();
+      connection.query(
+        'SELECT * FROM products WHERE item_id=' + itemId,
+        function(err, selectedItem) {
+          if (err) throw err;
+          if (selectedItem[0].StockQuantity - quantity >= 0) {
+            // console.log("Bamazon's shelves have enough of that item ("selectedItem[0].ProductName + ")!");
+            console.log(
+              'Quantity in Stock: ' +
+                selectedItem[0].StockQuantity +
+                ' Order Quantity: ' +
+                quantity
+            );
+            console.log(
+              'You will be charged ' +
+                order.quantity * selectedItem[0].Price +
+                ' dollars.  Thank you for shopping at Bamazon.'
+            );
+            connection.query(
+              'UPDATE products SET StockQuantity=? WHERE id=?',
+              [selectedItem[0].StockQuantity - quantity, itemId],
+              function(err, inventory) {
+                if (err) throw err;
+                queryProducts();
+              }
+            );
+          } else {
+            console.log(
+              'Insufficient inventory to fulfill this order. Please order less of that item, as Bamazon only has ' +
+                selectedItem[0].StockQuantity +
+                ' ' +
+                selectedItem[0].ProductName +
+                ' in stock at this moment.'
+            );
+            queryProducts();
+          }
         }
-      });
+      );
     });
 }
 
